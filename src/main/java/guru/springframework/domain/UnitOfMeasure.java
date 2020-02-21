6ac48105a7131ec6fa614cb682cbb318cefb3edc
sebas <daum.sebastian@gmail.com>
2020-02-21 14:19:03 +0100

    adding 1-1 relationship between ingredient and unit of measure

---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,33 @@
+package guru.springframework.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String unitNaming;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUnitNaming() {
+        return unitNaming;
+    }
+
+    public void setUnitNaming(String unitNaming) {
+        this.unitNaming = unitNaming;
+    }
+}
